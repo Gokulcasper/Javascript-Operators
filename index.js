a3bf2508d5a31e754a5d5c6e35d1db9f793f9e29
Gokulcasper <ronitcasper215@gmail.com>
2024-06-20 12:21:44 +0530

    Assignment and Arithmetic operator

---
+++ index.js
@@ -0,0 +1,38 @@
+/* -----------Assignment Operators ---------------- */
+
+// let x = 5;
+// alert("Check the values on Consoloe");
+// x++;
+// x = x + 5;
+// x += 5;
+// x *= 5;
+// x -= 5;
+// x /= 5;
+// console.log(x);
+
+
+/* -------- Arithmetic Operator-------------------- */
+
+let number1 = 5;
+let number2 = 10;
+// console.log(number1 + number2);
+// console.log(number1 - number2);
+// console.log(number1 / number2);
+// console.log(number1 % number2);
+// console.log(number1 * number2);
+// console.log(number1 ** number2);
+
+// Increment (++)
+// console.log(++number1);
+// console.log(number1);
+//or
+// console.log(number1++);
+// console.log(number1);
+
+// Decrement (++)
+console.log(--number1);
+console.log(number1);
+//or
+console.log(number1--);
+console.log(number1);
+
