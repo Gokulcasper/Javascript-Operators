1a8a4d2785d58246323d0c7e8bdfc9e767b277f7
Gokulcasper <ronitcasper215@gmail.com>
2024-06-22 13:04:48 +0530

    comparation operator

--- index.js
+++ index.js
@@ -13,8 +13,8 @@
 
 /* -------- Arithmetic Operator-------------------- */
 
-let number1 = 5;
-let number2 = 10;
+// let number1 = 5;
+// let number2 = 10;
 // console.log(number1 + number2);
 // console.log(number1 - number2);
 // console.log(number1 / number2);
@@ -30,9 +30,22 @@ let number2 = 10;
 // console.log(number1);
 
 // Decrement (++)
-console.log(--number1);
-console.log(number1);
-//or
-console.log(number1--);
-console.log(number1);
+// console.log(--number1);
+// console.log(number1);
+// //or
+// console.log(number1--);
+// console.log(number1);
 
+// Comparision Operator
+let x = 5;
+//relational operator
+// console.log(x < 5);
+// console.log(x > 5);
+//Equality Operator
+// console.log(x == 5);
+// console.log(x !=5);
+//String comparations
+// console.log('gokul'< 'gopal')
+//comparation of diffe
+console.log('1'< 5);
+console.log(true ==0);
